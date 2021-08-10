75cc98fae52a51aa3973e52418bca5d80ea7f8ee
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-08-10 19:49:23 -0400

    created footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import YellowSection from './components/YellowSection';
 import Perks from './components/Perks';
 import Reviews from './components/Reviews';
 import Purchase from './components/Purchase';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Perks/>
       <Reviews/>
       <Purchase/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="email">
+                <i class="fas fa-envelope"></i>
+                <p>support@experienceconcerts.co</p>
+            </div>
+            <h4>EXP|CON</h4>
+            <small>2019 © All Rights Reserved | Speer Technologies Incorporated</small>
+        </footer>
+    )
+}
+
+export default Footer;
