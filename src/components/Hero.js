66028b0ea976dd787c95a022ca6726944be81e12
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-08-10 16:59:52 -0400

    created hero section

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <h2>INTERACTIVE CONCERT EXPERIENCE</h2>
+            <p>Experience your favourite artists like never before and from the comfort of your own home.</p>
+            <button>TRY IT NOW</button>
+        </div>
+    )
+}
+
+export default Hero;
