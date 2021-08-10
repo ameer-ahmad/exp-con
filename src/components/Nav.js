8f9a967586e274488332e557983b9c1a8b1e8745
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-08-10 16:09:46 -0400

    created header

---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <header>
+            <div className="menu-btn">
+                <div className="menu-btn-burger"></div>
+            </div>
+            <h1 className="logo">EXP|CON</h1>
+        </header>
+    )
+}
+
+export default Nav;
