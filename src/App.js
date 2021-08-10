76b1d9fab872217ca6aa287a6e5305b9db6d7cd7
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-08-10 18:20:48 -0400

    created red section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Nav from './components/Nav';
 import Hero from './components/Hero';
+import RedSection from './components/RedSection';
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Hero/>
+      <RedSection/>
     </div>
   );
 }
---
+++ src/components/RedSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import speaker1 from '../img/speaker1.png';
+import speaker2 from '../img/speaker2.png';
+
+const RedSection = () => {
+    return (
+        <div className="redSection">
+            <h2>SUPERIOR SOUND</h2>
+            <h3>Experience live versions of your favourite songs.</h3>
+            <button className="seeDemo">SEE DEMO</button>
+            <button className="tryNow">TRY IT NOW</button>
+            <img src={speaker1} className="speaker1"/>
+            <img src={speaker2} className="speaker2" />
+        </div>
+    )
+}
+
+export default RedSection;
