50133912780580f77f81de49da892a837f5d13de
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-08-12 16:16:26 -0400

    created custom cursor on red section hover

--- src/components/RedSection.js
+++ src/components/RedSection.js
@@ -3,14 +3,15 @@ import speaker1 from '../img/speaker1.png';
 import speaker2 from '../img/speaker2.png';
 
 const RedSection = () => {
+
     return (
         <div className="redSection">
             <h2>SUPERIOR SOUND</h2>
             <h3>Experience live versions of your favourite songs.</h3>
             <button className="seeDemo">SEE DEMO</button>
             <button className="tryNow">TRY IT NOW</button>
-            <img src={speaker1} className="speaker1"/>
-            <img src={speaker2} className="speaker2" />
+            <img src={speaker1} className="speaker1 cursor"/>
+            <img src={speaker2} className="speaker2 cursor"/>
         </div>
     )
 }
