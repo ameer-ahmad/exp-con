fd0b7ed1e69fceb1aa1fc4162a2c602f65215d19
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-08-12 15:25:26 -0400

    implemented hamburger menu function

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import {Link} from 'react-router-dom'
+import { Swiper, SwiperSlide } from "swiper/react";
+import bgImage1 from '../img/bgImage1.png';
+import bgImage2 from '../img/bgImage2.png';
+import bgImage3 from '../img/bgImage3.png';
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
+import "swiper/components/navigation/navigation.min.css"
+
+import SwiperCore, {
+    Pagination,Autoplay
+  } from 'swiper/core';
+
+SwiperCore.use([Autoplay, Pagination]);
 
 const Hero = () => {
+
     return (
-        <div className="hero">
-            <h2>INTERACTIVE CONCERT EXPERIENCE</h2>
-            <p>Experience your favourite artists like never before and from the comfort of your own home.</p>
-            <Link to="/pricing"><button>TRY IT NOW</button></Link>
-        </div>
+            <Swiper autoplay={{"delay": 10000, "disableOnInteraction": false}} pagination={{"clickable":true}} loop={true} className="mySwiper hero">
+                <SwiperSlide><img src={bgImage1} alt="concert" /></SwiperSlide>
+                <SwiperSlide><img src={bgImage2} alt="concert" /></SwiperSlide>
+                <SwiperSlide><img src={bgImage3} alt="concert" /></SwiperSlide>
+                <div className="content">
+                    <h2>INTERACTIVE CONCERT EXPERIENCE</h2>
+                    <p>Experience your favourite artists like never before and from the comfort of your own home.</p>
+                    <button>TRY IT NOW</button>
+                </div>
+            </Swiper>
     )
 }
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Nav = () => {
+    const [isOpen, setOpen] = useState(false);
+    const [menuClass, setMenuClass] = useState("burgerMenu hide");
+
+    const toggleMenu = () => {
+        if (isOpen) {
+            setMenuClass("burgerMenu hide");
+            setOpen(!isOpen);
+        } else {
+            setMenuClass("burgerMenu");
+            setOpen(!isOpen);
+        }
+    }
+
     return (
         <header>
-            <div className="menu-btn">
+            <div className="menu-btn" onClick={toggleMenu}>
                 <div className="menu-btn-burger"></div>
             </div>
             <h1 className="logo">EXP|CON</h1>
+            <div className={menuClass}>
+                <ul>
+                    <li>WHAT IS IT</li>
+                    <li>PERKS</li>
+                    <li>PRICING</li>
+                </ul>
+            </div>
         </header>
     )
 }
